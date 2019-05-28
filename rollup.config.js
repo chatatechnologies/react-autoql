fb5027e7a3e712c1b22466662f0bbc061fb9e94d
Nicole Moore <nmoore@chata.ai>
2019-05-28 15:12:48 -0600

    add babel and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,45 @@
-const dist = 'dist'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 
-export default {
+const dist = 'dist'
+const bundle = 'bundle'
+
+const production = !process.env.ROLLUP_WATCH
+
+const common = {
   input: 'src/index.js',
-  output: [
-    {
-      file: `${dist}/bundle.cjs.js`,
-      format: 'cjs'
-    },
-    {
-      file: `${dist}/bundle.esm.js`,
-      // use ES modules to support tree-shaking
-      format: 'esm'
-    },
-    {
-      name: 'ChataAI',
-      file: `${dist}/bundle.umd.js`,
-      format: 'umd'
-    }
+  external: ['react'],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    production && terser()
   ]
 }
+
+const outputs = [
+  {
+    file: `${dist}/${bundle}.cjs.js`,
+    format: 'cjs'
+  },
+  {
+    file: `${dist}/${bundle}.esm.js`,
+    // use ES modules to support tree-shaking
+    format: 'esm'
+  },
+  {
+    name: 'ChataAI',
+    file: `${dist}/${bundle}.umd.js`,
+    globals: {
+      react: 'React'
+    },
+    format: 'umd'
+  }
+]
+
+export default outputs.map(output => ({
+  ...common,
+  output
+}))
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello' + ' Nikki')
+import React from 'react'
+
+const Spinner = props => <h1>Spinner</h1>
+
+export default Spinner
