33aae9ce49271ed7d436fa213172b6263870d543
Nicole Moore <nmoore@chata.ai>
2019-05-28 18:34:40 -0600

    add pre-commit hook for jest

--- src/components/ChatDrawer/ChatDrawer.test.js
+++ src/components/ChatDrawer/ChatDrawer.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { ChatDrawer } from '../..'
+import { exportAllDeclaration } from '@babel/types'
 
 it('renders correctly', () => {
   const tree = renderer.create(<ChatDrawer />).toJSON()
+  expect(tree).toMatchSnapshot()
 })
