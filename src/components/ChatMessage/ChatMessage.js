f12852f06d825b674929b90a225af1aa9e77ae9f
Nicole Moore <nmoore@chata.ai>
2019-08-12 18:50:46 -0600

    separate svg for display icons into its own file

--- src/components/ChatMessage/ChatMessage.js
+++ src/components/ChatMessage/ChatMessage.js
@@ -6,6 +6,17 @@ import uuid from 'uuid'
 
 import { ResponseRenderer } from '../ResponseRenderer'
 
+import {
+  tableIcon,
+  pivotTableIcon,
+  columnChartIcon,
+  barChartIcon,
+  lineChartIcon,
+  pieChartIcon,
+  heatmapIcon,
+  bubbleChartIcon
+} from '../../svgIcons.js'
+
 export default class ChatMessage extends React.Component {
   supportedDisplayTypes = []
 
@@ -219,6 +230,22 @@ export default class ChatMessage extends React.Component {
     )
   }
 
+  createDisplayTypeButton = (displayType, name, icon) => {
+    if (this.showDisplayTypeButton(displayType)) {
+      return (
+        <button
+          onClick={() => this.switchView(displayType)}
+          className="chata-toolbar-btn"
+          data-tip={name}
+          data-for="chata-toolbar-btn-tooltip"
+        >
+          {icon}
+        </button>
+      )
+    }
+    return null
+  }
+
   renderLeftToolbar = () => {
     if (!this.supportedDisplayTypes || this.supportedDisplayTypes.length <= 1) {
       return null
@@ -231,32 +258,7 @@ export default class ChatMessage extends React.Component {
     ) {
       return (
         <div className="chat-message-toolbar left">
-          {this.showDisplayTypeButton('table') && (
-            <button
-              onClick={() => this.switchView('table')}
-              className="chata-toolbar-btn"
-              data-tip="Table"
-              data-for="chata-toolbar-btn-tooltip"
-            >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M8,0.8c2.3,0,4.6,0,6.9,0c0.8,0,1.1,0.3,1.1,1.1c0,4,0,7.9,0,11.9c0,0.8-0.3,1.1-1.1,1.1c-4.6,0-9.3,0-13.9,0
-c-0.7,0-1-0.3-1-1c0-4,0-8,0-12c0-0.7,0.3-1,1-1C3.4,0.8,5.7,0.8,8,0.8L8,0.8z M5,11.1H1v2.7h4V11.1L5,11.1z M10,13.8v-2.7H6v2.7
-L10,13.8L10,13.8z M11,13.8h4v-2.7h-4V13.8L11,13.8z M1.1,7.5v2.7h4V7.5H1.1L1.1,7.5z M11,10.2c1.3,0,2.5,0,3.8,0
-c0.1,0,0.2-0.1,0.2-0.2c0-0.8,0-1.7,0-2.5h-4C11,8.4,11,9.3,11,10.2L11,10.2z M6,10.1h4V7.5H6V10.1L6,10.1z M5,6.6V3.9H1
-c0,0.8,0,1.6,0,2.4c0,0.1,0.2,0.2,0.3,0.2C2.5,6.6,3.7,6.6,5,6.6L5,6.6z M6,6.5h4V3.9H6V6.5L6,6.5z M14.9,6.5V3.9h-4v2.7L14.9,6.5
-L14.9,6.5z"
-                />
-              </svg>
-            </button>
-          )}
+          {this.createDisplayTypeButton('table', 'Table', tableIcon)}
           {this.showDisplayTypeButton('pivot_table') && (
             <button
               onClick={() => this.switchView('pivot_table')}
@@ -264,21 +266,7 @@ L14.9,6.5z"
               data-tip="Pivot Table"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M8,0.7c2.3,0,4.6,0,6.9,0C15.7,0.7,16,1,16,1.8c0,4,0,7.9,0,11.9c0,0.8-0.3,1.1-1.1,1.1c-4.6,0-9.3,0-13.9,0
-	c-0.7,0-1-0.3-1-1c0-4,0-8,0-12c0-0.7,0.3-1,1-1C3.4,0.7,5.7,0.7,8,0.7L8,0.7z M5.1,6.4h4.4V3.8H5.1V6.4L5.1,6.4z M14.9,6.4V3.8
-	h-4.4v2.7L14.9,6.4L14.9,6.4z M5.1,10.1h4.4V7.4H5.1V10.1L5.1,10.1z M14.9,10.1V7.4h-4.4v2.7H14.9L14.9,10.1z M5.1,13.7h4.4V11H5.1
-	V13.7L5.1,13.7z M14.9,13.7V11h-4.4v2.7L14.9,13.7L14.9,13.7z"
-                />
-              </svg>
+              {pivotTableIcon}
             </button>
           )}
           {this.showDisplayTypeButton('column') && (
@@ -288,21 +276,7 @@ L14.9,6.5z"
               data-tip="Column Chart"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M12.6,0h-2.4C9.4,0,8.8,0.6,8.8,1.4v2.7c0,0,0,0,0,0H6.3c-0.8,0-1.4,0.6-1.4,1.4v3.2c0,0-0.1,0-0.1,0H2.4
-                    C1.6,8.7,1,9.4,1,10.1v4.5C1,15.4,1.6,16,2.4,16h2.4c0,0,0.1,0,0.1,0h1.3c0,0,0.1,0,0.1,0h2.4c0,0,0.1,0,0.1,0H10c0,0,0.1,0,0.1,0
-                    h2.4c0.8,0,1.4-0.6,1.4-1.4V1.4C14,0.6,13.3,0,12.6,0z M6.3,5.5h2.4v9.1H6.3V5.5z M2.4,10.1h2.4v4.5H2.4V10.1z M12.6,14.6h-2.4V1.4
-                    h2.4V14.6z"
-                />
-              </svg>
+              {columnChartIcon}
             </button>
           )}
           {this.showDisplayTypeButton('bar') && (
@@ -312,20 +286,7 @@ L14.9,6.5z"
               data-tip="Bar Chart"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M14.6,1.6H1.4C0.6,1.6,0,2.2,0,3v2.4v0.1v1.2v0.1v2.4v0.1v1.3v0.1v2.4c0,0.8,0.6,1.4,1.4,1.4h4.5
-                  c0.7,0,1.4-0.6,1.4-1.4v-2.4v-0.1h3.2c0.8,0,1.4-0.6,1.4-1.4V6.7l0,0h2.7c0.8,0,1.4-0.6,1.4-1.4V2.9C16,2.2,15.4,1.5,14.6,1.6z
-                   M1.4,9.2V6.8h9.1v2.4H1.4z M1.4,13.1v-2.4h4.5v2.4H1.4z M14.6,2.9v2.4H1.4V2.9H14.6z"
-                />
-              </svg>
+              {barChartIcon}
             </button>
           )}
           {this.showDisplayTypeButton('line') && (
@@ -335,20 +296,7 @@ L14.9,6.5z"
               data-tip="Line Chart"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M1,12.2c-0.2,0-0.3-0.1-0.5-0.2c-0.3-0.3-0.3-0.7,0-1l3.8-3.9C4.5,7,4.7,7,4.9,7s0.4,0.1,0.5,0.3l2.3,3l6.8-7.1
-            c0.3-0.3,0.7-0.3,1,0c0.3,0.3,0.3,0.7,0,1l-7.3,7.7C8,11.9,7.8,12,7.6,12s-0.4-0.1-0.5-0.3l-2.3-3L1.5,12C1.4,12.2,1.2,12.2,1,12.2z
-            "
-                />
-              </svg>
+              {lineChartIcon}
             </button>
           )}
           {this.showDisplayTypeButton('pie') && (
@@ -358,28 +306,7 @@ L14.9,6.5z"
               data-tip="Pie Chart"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="chart-icon-svg-0"
-                  d="M12.6,8.5H7.4V3.3c0-0.4-0.3-0.7-0.7-0.7c-0.9,0-1.8,0.2-2.6,0.5C3.3,3.5,2.6,3.9,2,4.6
-              C1.3,5.2,0.9,5.9,0.5,6.7C0.2,7.5,0,8.4,0,9.3c0,0.9,0.2,1.8,0.5,2.6C0.9,12.7,1.3,13.4,2,14c0.6,0.6,1.3,1.1,2.1,1.4
-              C4.9,15.8,5.8,16,6.7,16c0.9,0,1.8-0.2,2.6-0.5c0.8-0.3,1.5-0.8,2.1-1.4c0.6-0.6,1.1-1.3,1.4-2.1c0.3-0.8,0.5-1.7,0.5-2.6
-              C13.4,8.9,13,8.5,12.6,8.5z M6.7,10h5.2c-0.2,1.1-0.7,2.1-1.5,3c-1,1-2.3,1.5-3.7,1.5C5.3,14.5,4,14,3,13c-1-1-1.5-2.3-1.5-3.7
-              C1.5,7.9,2,6.6,3,5.6c0.8-0.8,1.8-1.3,3-1.5v5.2C6,9.7,6.3,10,6.7,10z"
-                />
-                <path
-                  className="chart-icon-svg-0"
-                  d="M15.5,4.1C15.1,3.3,14.7,2.6,14,2c-0.6-0.6-1.3-1.1-2.1-1.4C11.1,0.2,10.2,0,9.3,0C8.9,0,8.6,0.3,8.6,0.7v6
-              c0,0.4,0.3,0.7,0.7,0.7h6c0.4,0,0.7-0.3,0.7-0.7C16,5.8,15.8,4.9,15.5,4.1z M10,6V1.5c1.1,0.2,2.2,0.7,3,1.5c0.8,0.8,1.3,1.8,1.5,3
-              H10z"
-                />
-              </svg>
+              {pieChartIcon}
             </button>
           )}
           {this.showDisplayTypeButton('heatmap') && (
@@ -389,59 +316,7 @@ L14.9,6.5z"
               data-tip="Heatmap"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  className="hm0"
-                  d="M12,16h2.5c0.8,0,1.5-0.7,1.5-1.5v-2.4l-4,0V16z"
-                />
-                <polygon className="hm1" points="8,4.1 8,0 4,0 4,4.1 " />
-                <path
-                  className="hm2"
-                  d="M4,4.1V0L1.5,0C0.7,0,0,0.7,0,1.5l0,2.6h0l0,0L4,4.1z"
-                />
-                <polygon
-                  className="hm3"
-                  points="8,4.1 8,4.1 8,4.1 4,4.1 4,8.1 8,8.2 "
-                />
-                <polygon className="hm2" points="0,4.1 0,8.1 4,8.1 4,4.1 " />
-                <polygon className="hm1" points="4,4.1 8,4.1 8,4.1 " />
-                <polygon className="hm1" points="4,16 8,16 8,12.1 4,12.1 " />
-                <path
-                  className="hm0"
-                  d="M0,12.1v2.5C0,15.3,0.7,16,1.5,16H4v-3.9L0,12.1z"
-                />
-                <polygon className="hm0" points="0,12.1 4,12.1 4,8.2 0,8.2 " />
-                <polygon className="hm4" points="8,12.1 8,8.2 4,8.2 4,12.1 " />
-                <polygon
-                  className="hm2"
-                  points="16,4.1 16,4.1 16,4.1 12,4.1 12,8.2 16,8.2 "
-                />
-                <path
-                  className="hm0"
-                  d="M16,4.1l0-2.6C16,0.7,15.3,0,14.5,0L12,0v4.1L16,4.1z"
-                />
-                <polygon
-                  className="hm4"
-                  points="12,4.1 12,0 8,0 8,4.1 8,4.1 8,4.1 "
-                />
-                <polygon className="hm5" points="12,4.1 8,4.1 8,4.1 " />
-                <polygon className="hm6" points="12,4.1 16,4.1 16,4.1 " />
-                <polygon
-                  className="hm2"
-                  points="12,12.1 16,12.1 16,8.2 12,8.2 "
-                />
-                <polygon
-                  className="hm1"
-                  points="12,8.2 8,8.2 8,12.1 12,12.1 "
-                />
-                <polygon className="hm1" points="8,12.1 8,16 12,16 12,12.1 " />
-              </svg>
+              {heatmapIcon}
             </button>
           )}
           {this.showDisplayTypeButton('bubble') && (
@@ -451,38 +326,7 @@ L14.9,6.5z"
               data-tip="Bubble Chart"
               data-for="chata-toolbar-btn-tooltip"
             >
-              <svg
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 16 16"
-              >
-                <circle
-                  className="chart-icon-svg-0"
-                  cx="7.7"
-                  cy="11.1"
-                  r="1.2"
-                />
-                <circle
-                  className="chart-icon-svg-0"
-                  cx="2.6"
-                  cy="8.8"
-                  r="2.6"
-                />
-                <circle
-                  className="chart-icon-svg-0"
-                  cx="11.7"
-                  cy="4.3"
-                  r="4.3"
-                />
-                <circle
-                  className="chart-icon-svg-0"
-                  cx="1.8"
-                  cy="14.8"
-                  r="1.2"
-                />
-              </svg>
+              {bubbleChartIcon}
             </button>
           )}
         </div>
