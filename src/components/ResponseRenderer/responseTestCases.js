3a9b512323e9c573e9d8ad045fd72b80c82e8794
Nicole Moore <nmoore@chata.ai>
2019-09-04 10:20:08 -0600

    add test case with null rows

--- src/components/ResponseRenderer/responseTestCases.js
+++ src/components/ResponseRenderer/responseTestCases.js
@@ -1,1 +1,15 @@
-export default [{}, { data: { displayType: 'table' } }]
+export default [
+  {},
+  { data: { displayType: 'table' } },
+  {
+    data: {
+      columns: [],
+      displayType: null,
+      interpretation: 'all invoices for invoice of this month ',
+      queryId: 'q_VzB9irC-RpKY-13XwgHrrw',
+      rows: null
+    },
+    message: '',
+    referenceId: '1.1.0'
+  }
+]
