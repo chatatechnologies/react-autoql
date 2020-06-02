7ab51c7cb86bc92b875fd89c8c840190faff868c
NikkiMeganMoore <nmoore@chata.ai>
2020-06-02 16:42:35 -0600

    chore(cleanup): remove Storybook

--- stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Button } from '@storybook/react/demo'
-import { DataMessenger } from '../src'
-
-storiesOf('Chat Components', module)
-  .add('Data Messenger', () => <DataMessenger isVisible />)
-  .add('Chat Embedded', () => <div />)
