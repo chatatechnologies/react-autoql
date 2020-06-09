f57db47541cc8876e7c3ecc29f4d1adab82e2489
NikkiMeganMoore <nmoore@chata.ai>
2020-06-09 11:22:34 -0600

    test(config): fix jest config env variables

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,21 @@ module.exports = {
   verbose: false,
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '^.+\\.(css|scss|less)$': 'jest-css-modules'
+    '^.+\\.(css|scss|less)$': 'jest-css-modules',
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/mocks/fileMock.js'
+      '<rootDir>/mocks/fileMock.js',
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/media.js',
-    '\\.(css|scss)$': '<rootDir>/mocks/styles.js'
+    '\\.(css|scss)$': '<rootDir>/mocks/styles.js',
   },
   globals: {
-    NODE_ENV: 'test'
+    NODE_ENV: 'test',
+    React: 'React',
   },
   moduleFileExtensions: ['js', 'jsx'],
-  moduleDirectories: ['node_modules']
+  moduleDirectories: ['node_modules'],
 }
