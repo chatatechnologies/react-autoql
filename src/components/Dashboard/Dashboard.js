de33eb69f5562b7fe42b5b74751f59baa8a4c12e
Nikki Moore <nmoore@chata.ai>
2020-09-04 14:25:47 -0600

    fix(dashboards): fix "no data supplied" error in split view

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -459,7 +459,7 @@ class Dashboard extends React.Component {
       let dataConfig
       if (tile && this.state.isDrilldownSecondHalf) {
         title = tile.secondQuery
-        queryResponse = tile.secondQueryResponse
+        queryResponse = tile.secondQueryResponse || tile.queryResponse
         displayType = tile.secondDisplayType
         dataConfig = tile.secondDataConfig
       } else if (tile && !this.state.isDrilldownSecondHalf) {
