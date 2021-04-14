3ef5ace4fa64518af2ba1e00a6d30619fdecb79b
Melissa <57367085+melpicazo@users.noreply.github.com>
2021-04-14 09:39:53 -0600

    fixing compile error

--- src/components/DataMessenger/DataMessenger.js
+++ src/components/DataMessenger/DataMessenger.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import LocalizedStrings from 'react-localization'
 import { number, bool, string, func, shape, array, oneOfType } from 'prop-types'
 import uuid from 'uuid'
 import Drawer from 'rc-drawer'
@@ -44,6 +45,79 @@ import { runDrilldown, fetchQueryTips } from '../../js/queryService'
 // Styles
 import 'rc-drawer/assets/index.css'
 import './DataMessenger.scss'
+
+let strings = new LocalizedStrings({
+  en: {
+    dataMessenger: "Data Messenger",
+    introPrompt: "Some things you can ask me: ",
+    explorePrompt: "Use Explore Queries to further explore the possibilities.",
+    run: "We run on AutoQL by Chata",
+    queryPrompt: "Type your queries here ",
+    seeMore: "See more",
+    clearDataResponses: "Clear data responses",
+    closeDataMessenger: "Close Data Messenger",
+    exploreQueries: "Explore Queries",
+    searchQueries: "Search relevant queries by topic",
+    discoverPrompt: "Discover what you can ask by entering a topic in the search bar above. Simply click on any of the returned options to run the query in Data Messenger.",
+    reportAProblem: "Report a problem",
+    deleteDataResponse: "Delete data response",
+    table: "Table",
+    barChart: "Bar Chart",
+    columnChart: "Column Chart",
+    lineChart: "Line Chart",
+    pieChart: "Pie Chart",
+    heatMap: "Heat Map",
+    bubbleChart: "Bubble Chart",
+    stackedColumnChart: "Stacked Column Chart",
+    stackedBarChart: "Stacked Bar Chart",
+    stackedAreaChart: "Stacked Area Chart",
+    downloadAsPNG: "Download as a PNG",
+    filterTable: "Filter table",
+    showHideColumns: "Show/hide columns",
+    downloadAsCSV: "Download as a CSV",
+    copyTableToClipboard: "Copy table to clipboard",
+    filterColumn: "Filter column",
+    columnName: "Column Name",
+    visibility: "Visibility",
+    cancel: "Cancel",
+    apply: "Apply"
+  },
+  sp: {
+    dataMessenger: "Data Messenger",
+    introPrompt: "Cosas que puedes preguntarme:",
+    explorePrompt: "Utiliza Explorar Consultas para ampliar las posibilidades.",
+    run: "Corremos en AutoQL de Chata",
+    queryPrompt: "Escribe tus consultas aquí",
+    seeMore: "Ver más",
+    clearDataResponses: "Limpiar respuestas",
+    closeDataMessenger: "Cerrar Data mesenger",
+    exploreQueries: "Explorar consultas",
+    searchQueries: "Buscar consultas relevantes por tema",
+    discoverPrompt: "Descubre lo que puede preguntar ingresando un tema en la barra de búsqueda de arriba. Da clic en cualquiera de las opciones devueltas para ejecutar la consulta en Data Messenger.",
+    reportAProblem: "Informar de un problema",
+    deleteDataResponse: "Eliminar respuesta de datos",
+    table: "Tabla",
+    barChart: "Gráfico de barras",
+    columnChart: "Gráfico de columnas",
+    lineChart: "Gráfico de linea",
+    pieChart: "Gráfico circular",
+    heatMap: "Mapa de calor",
+    bubbleChart: "Gráfico de burbujas",
+    stackedColumnChart: "Gráfico de columnas apiladas",
+    stackedBarChart: "Gráfico de barras apiladas",
+    stackedAreaChart: "Gráfico de área apilada",
+    downloadAsPNG: "Descargar como PNG",
+    filterTable: "Tabla de filtros",
+    showHideColumns: "Mostrar/ocultar columnas",
+    downloadAsCSV: "Descargar como CSV",
+    copyTableToClipboard: "Copiar tabla al portapapelas",
+    filterColumn: "Columna de filtro",
+    columnName: "Nombre de columna",
+    visibility: "Visibilidad",
+    cancel: "Cancelar",
+    apply: "Aplicar"
+  }
+});
 
 export default class DataMessenger extends React.Component {
   static propTypes = {
@@ -118,10 +192,10 @@ export default class DataMessenger extends React.Component {
     autoChartAggregations: true,
 
     // Callbacks
-    onHandleClick: () => {},
-    onVisibleChange: () => {},
-    onErrorCallback: () => {},
-    onSuccessAlert: () => {},
+    onHandleClick: () => { },
+    onVisibleChange: () => { },
+    onErrorCallback: () => { },
+    onSuccessAlert: () => { },
   }
 
   state = {
@@ -294,7 +368,7 @@ export default class DataMessenger extends React.Component {
         content: this.props.introMessage
           ? `${this.props.introMessage}`
           : `Hi ${this.props.userDisplayName ||
-              'there'}! Let’s dive into your data. What can I help you discover today?`,
+          'there'}! Let’s dive into your data. What can I help you discover today?`,
       }),
     ]
 
@@ -337,8 +411,8 @@ export default class DataMessenger extends React.Component {
               draggable="false"
             />
           ) : (
-            <Icon type="react-autoql-bubbles-outlined" size={26} />
-          )}
+              <Icon type="react-autoql-bubbles-outlined" size={26} />
+            )}
         </div>
       )
     }
@@ -634,9 +708,8 @@ export default class DataMessenger extends React.Component {
               </div>
               {this.props.enableExploreQueriesTab && (
                 <div
-                  className={`tab${
-                    page === 'explore-queries' ? ' active' : ''
-                  } react-autoql-explore-queries`}
+                  className={`tab${page === 'explore-queries' ? ' active' : ''
+                    } react-autoql-explore-queries`}
                   onClick={() =>
                     this.setState({ activePage: 'explore-queries' })
                   }
@@ -651,9 +724,8 @@ export default class DataMessenger extends React.Component {
                 getAutoQLConfig(getAutoQLConfig(this.props.autoQLConfig))
                   .enableNotifications && (
                   <div
-                    className={`tab${
-                      page === 'notifications' ? ' active' : ''
-                    } react-autoql-notifications`}
+                    className={`tab${page === 'notifications' ? ' active' : ''
+                      } react-autoql-notifications`}
                     onClick={() => {
                       if (this.notificationBadgeRef) {
                         this.notificationBadgeRef.resetCount()
