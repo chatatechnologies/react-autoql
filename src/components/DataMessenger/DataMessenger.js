8ab156f8cd9621c82f39931808e9e4a58b08b15f
Ben-CHATA <bwozak@chata.ai>
2021-04-29 14:16:34 -0600

    fix(datamessenger): messenger placement bug fixed
    Fixed a bug where the data Messenger would break with a placement value of bottom or top
    
    AC-971

--- src/components/DataMessenger/DataMessenger.js
+++ src/components/DataMessenger/DataMessenger.js
@@ -49,76 +49,78 @@ import './DataMessenger.scss'
 
 let lang = new LocalizedStrings({
   en: {
-    introPrompt: "Some things you can ask me: ",
-    use: "Use",
-    exploreQueries: "Explore Queries",
-    explorePrompt: "to further explore the possibilities.",
-    run: "We run on AutoQL by Chata",
-    queryPrompt: "Type your queries here ",
-    seeMore: "See more",
-    clearDataResponses: "Clear data responses",
-    closeDataMessenger: "Close Data Messenger",
-    searchQueries: "Search relevant queries by topic",
-    discoverPrompt: "Discover what you can ask by entering a topic in the search bar above. Simply click on any of the returned options to run the query in Data Messenger.",
-    reportAProblem: "Report a problem",
-    deleteDataResponse: "Delete data response",
-    table: "Table",
-    barChart: "Bar Chart",
-    columnChart: "Column Chart",
-    lineChart: "Line Chart",
-    pieChart: "Pie Chart",
-    heatMap: "Heat Map",
-    bubbleChart: "Bubble Chart",
-    stackedColumnChart: "Stacked Column Chart",
-    stackedBarChart: "Stacked Bar Chart",
-    stackedAreaChart: "Stacked Area Chart",
-    downloadAsPNG: "Download as a PNG",
-    filterTable: "Filter table",
-    showHideColumns: "Show/hide columns",
-    downloadAsCSV: "Download as a CSV",
-    copyTableToClipboard: "Copy table to clipboard",
-    filterColumn: "Filter column",
-    columnName: "Column Name",
-    visibility: "Visibility",
-    cancel: "Cancel",
-    apply: "Apply"
+    introPrompt: 'Some things you can ask me: ',
+    use: 'Use',
+    exploreQueries: 'Explore Queries',
+    explorePrompt: 'to further explore the possibilities.',
+    run: 'We run on AutoQL by Chata',
+    queryPrompt: 'Type your queries here ',
+    seeMore: 'See more',
+    clearDataResponses: 'Clear data responses',
+    closeDataMessenger: 'Close Data Messenger',
+    searchQueries: 'Search relevant queries by topic',
+    discoverPrompt:
+      'Discover what you can ask by entering a topic in the search bar above. Simply click on any of the returned options to run the query in Data Messenger.',
+    reportAProblem: 'Report a problem',
+    deleteDataResponse: 'Delete data response',
+    table: 'Table',
+    barChart: 'Bar Chart',
+    columnChart: 'Column Chart',
+    lineChart: 'Line Chart',
+    pieChart: 'Pie Chart',
+    heatMap: 'Heat Map',
+    bubbleChart: 'Bubble Chart',
+    stackedColumnChart: 'Stacked Column Chart',
+    stackedBarChart: 'Stacked Bar Chart',
+    stackedAreaChart: 'Stacked Area Chart',
+    downloadAsPNG: 'Download as a PNG',
+    filterTable: 'Filter table',
+    showHideColumns: 'Show/hide columns',
+    downloadAsCSV: 'Download as a CSV',
+    copyTableToClipboard: 'Copy table to clipboard',
+    filterColumn: 'Filter column',
+    columnName: 'Column Name',
+    visibility: 'Visibility',
+    cancel: 'Cancel',
+    apply: 'Apply',
   },
   sp: {
-    introPrompt: "Cosas que puedes preguntarme:",
-    use: "Utiliza",
-    exploreQueries: "Explorar Consultas",
-    explorePrompt: "para ampliar las posibilidades.",
-    run: "Corremos en AutoQL de Chata",
-    queryPrompt: "Escribe tus consultas aquí",
-    seeMore: "Ver más",
-    clearDataResponses: "Limpiar respuestas",
-    closeDataMessenger: "Cerrar Data mesenger",
-    searchQueries: "Buscar consultas relevantes por tema",
-    discoverPrompt: "Descubre lo que puede preguntar ingresando un tema en la barra de búsqueda de arriba. Da clic en cualquiera de las opciones devueltas para ejecutar la consulta en Data Messenger.",
-    reportAProblem: "Informar de un problema",
-    deleteDataResponse: "Eliminar respuesta de datos",
-    table: "Tabla",
-    barChart: "Gráfico de barras",
-    columnChart: "Gráfico de columnas",
-    lineChart: "Gráfico de linea",
-    pieChart: "Gráfico circular",
-    heatMap: "Mapa de calor",
-    bubbleChart: "Gráfico de burbujas",
-    stackedColumnChart: "Gráfico de columnas apiladas",
-    stackedBarChart: "Gráfico de barras apiladas",
-    stackedAreaChart: "Gráfico de área apilada",
-    downloadAsPNG: "Descargar como PNG",
-    filterTable: "Tabla de filtros",
-    showHideColumns: "Mostrar/ocultar columnas",
-    downloadAsCSV: "Descargar como CSV",
-    copyTableToClipboard: "Copiar tabla al portapapelas",
-    filterColumn: "Columna de filtro",
-    columnName: "Nombre de columna",
-    visibility: "Visibilidad",
-    cancel: "Cancelar",
-    apply: "Aplicar"
-  }
-});
+    introPrompt: 'Cosas que puedes preguntarme:',
+    use: 'Utiliza',
+    exploreQueries: 'Explorar Consultas',
+    explorePrompt: 'para ampliar las posibilidades.',
+    run: 'Corremos en AutoQL de Chata',
+    queryPrompt: 'Escribe tus consultas aquí',
+    seeMore: 'Ver más',
+    clearDataResponses: 'Limpiar respuestas',
+    closeDataMessenger: 'Cerrar Data mesenger',
+    searchQueries: 'Buscar consultas relevantes por tema',
+    discoverPrompt:
+      'Descubre lo que puede preguntar ingresando un tema en la barra de búsqueda de arriba. Da clic en cualquiera de las opciones devueltas para ejecutar la consulta en Data Messenger.',
+    reportAProblem: 'Informar de un problema',
+    deleteDataResponse: 'Eliminar respuesta de datos',
+    table: 'Tabla',
+    barChart: 'Gráfico de barras',
+    columnChart: 'Gráfico de columnas',
+    lineChart: 'Gráfico de linea',
+    pieChart: 'Gráfico circular',
+    heatMap: 'Mapa de calor',
+    bubbleChart: 'Gráfico de burbujas',
+    stackedColumnChart: 'Gráfico de columnas apiladas',
+    stackedBarChart: 'Gráfico de barras apiladas',
+    stackedAreaChart: 'Gráfico de área apilada',
+    downloadAsPNG: 'Descargar como PNG',
+    filterTable: 'Tabla de filtros',
+    showHideColumns: 'Mostrar/ocultar columnas',
+    downloadAsCSV: 'Descargar como CSV',
+    copyTableToClipboard: 'Copiar tabla al portapapelas',
+    filterColumn: 'Columna de filtro',
+    columnName: 'Nombre de columna',
+    visibility: 'Visibilidad',
+    cancel: 'Cancelar',
+    apply: 'Aplicar',
+  },
+})
 
 export default class DataMessenger extends React.Component {
   static propTypes = {
@@ -193,10 +195,10 @@ export default class DataMessenger extends React.Component {
     autoChartAggregations: true,
 
     // Callbacks
-    onHandleClick: () => { },
-    onVisibleChange: () => { },
-    onErrorCallback: () => { },
-    onSuccessAlert: () => { },
+    onHandleClick: () => {},
+    onVisibleChange: () => {},
+    onErrorCallback: () => {},
+    onSuccessAlert: () => {},
   }
 
   state = {
@@ -348,7 +350,8 @@ export default class DataMessenger extends React.Component {
             className="intro-qi-link"
             onClick={() => this.setState({ activePage: 'explore-queries' })}
           >
-            <Icon type="light-bulb" style={{ marginRight: '-3px' }} /> {lang.exploreQueries}
+            <Icon type="light-bulb" style={{ marginRight: '-3px' }} />{' '}
+            {lang.exploreQueries}
           </span>{' '}
           {lang.explorePrompt}
         </div>
@@ -368,7 +371,7 @@ export default class DataMessenger extends React.Component {
         content: this.props.introMessage
           ? `${this.props.introMessage}`
           : `Hi ${this.props.userDisplayName ||
-          'there'}! Let’s dive into your data. What can I help you discover today?`,
+              'there'}! Let’s dive into your data. What can I help you discover today?`,
       }),
     ]
 
@@ -411,8 +414,8 @@ export default class DataMessenger extends React.Component {
               draggable="false"
             />
           ) : (
-              <Icon type="react-autoql-bubbles-outlined" size={26} />
-            )}
+            <Icon type="react-autoql-bubbles-outlined" size={26} />
+          )}
         </div>
       )
     }
@@ -727,8 +730,9 @@ export default class DataMessenger extends React.Component {
               </div>
               {this.props.enableExploreQueriesTab && (
                 <div
-                  className={`tab${page === 'explore-queries' ? ' active' : ''
-                    } react-autoql-explore-queries`}
+                  className={`tab${
+                    page === 'explore-queries' ? ' active' : ''
+                  } react-autoql-explore-queries`}
                   onClick={() =>
                     this.setState({ activePage: 'explore-queries' })
                   }
@@ -743,8 +747,9 @@ export default class DataMessenger extends React.Component {
                 getAutoQLConfig(getAutoQLConfig(this.props.autoQLConfig))
                   .enableNotifications && (
                   <div
-                    className={`tab${page === 'notifications' ? ' active' : ''
-                      } react-autoql-notifications`}
+                    className={`tab${
+                      page === 'notifications' ? ' active' : ''
+                    } react-autoql-notifications`}
                     onClick={() => {
                       if (this.notificationBadgeRef) {
                         this.notificationBadgeRef.resetCount()
@@ -1277,12 +1282,11 @@ export default class DataMessenger extends React.Component {
   }
 
   setLanguage = () => {
-    var userLang = navigator.language || navigator.userLanguage;
-    if (userLang.includes("sp")) {
-      lang.setLanguage("sp")
-    }
-    else {
-      lang.setLanguage("en")
+    var userLang = navigator.language || navigator.userLanguage
+    if (userLang.includes('sp')) {
+      lang.setLanguage('sp')
+    } else {
+      lang.setLanguage('en')
     }
   }
 
@@ -1294,10 +1298,16 @@ export default class DataMessenger extends React.Component {
     const dataMessenger = document.getElementsByClassName(
       'drawer-content-wrapper'
     )[0]
-    if (chartToolTipElement && dataMessenger) {
-      chartToolTipElement.style.maxWidth = `${dataMessenger.style.width.match(
-        /\d+/g
-      )[0] - 70}px`
+    if (
+      (chartToolTipElement && dataMessenger) ||
+      this.props.placement !== 'top' ||
+      this.props.placement !== 'bottom'
+    ) {
+      if (_get(chartToolTipElement, 'style.maxWidth')) {
+        chartToolTipElement = `${_get(dataMessenger, 'style.width').match(
+          /\d+/g
+        )[0] - 70}px`
+      }
     }
 
     return (
