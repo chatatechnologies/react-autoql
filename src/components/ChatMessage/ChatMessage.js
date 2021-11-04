7391896dd18635b5d4bcfb6e827cdf1b3ec556b1
Ben-CHATA <benwozak@gmail.com>
2021-11-04 16:00:07 -0600

    condition improvements

--- src/components/ChatMessage/ChatMessage.js
+++ src/components/ChatMessage/ChatMessage.js
@@ -450,6 +450,50 @@ export default class ChatMessage extends React.Component {
     }
   }
 
+  /**
+   * ** WIP **
+   *
+   * Apply conditions to queries that contain them.
+   * @returns a list of conditions
+   */
+  renderLockedConditions = () => {
+    const { response } = this.props
+    const numRows = _get(response, 'data.data.rows.length')
+    const maxRowLimit = _get(response, 'data.data.row_limit')
+
+    // <div className="chat-condition-item-container">
+    //           {response.data.data.persistent_locked_conditions.map(
+    //             (condition) => {
+    //               return (
+    //                 <span>
+    //                   {' '}
+    //                   <button className="chat-condition-item">
+    //                     {condition}
+    //                   </button>
+    //                 </span>
+    //               )
+    //             }
+    //           )}
+    //         </div>
+
+    if (_get(response, 'data.data.persistent_locked_conditions.length') > 0) {
+      return (
+        <Icon
+          type="lock"
+          className={
+            maxRowLimit &&
+            numRows === maxRowLimit &&
+            !this.allColumnsAreHidden()
+              ? 'condition-info-icon-left-align'
+              : 'condition-info-icon'
+          }
+          data-tip={response.data.data.persistent_locked_conditions}
+          data-for="chart-element-tooltip"
+        />
+      )
+    }
+  }
+
   render = () => {
     const { chartWidth, chartHeight } = this.getChartDimensions()
     const messageHeight = this.getMessageHeight()
@@ -491,6 +535,7 @@ export default class ChatMessage extends React.Component {
             {this.props.isDataMessengerOpen && this.renderRightToolbar()}
             {this.props.isDataMessengerOpen && this.renderLeftToolbar()}
             {this.renderDataLimitWarning()}
+            {this.renderLockedConditions()}
           </div>
         </div>
       </ErrorBoundary>
