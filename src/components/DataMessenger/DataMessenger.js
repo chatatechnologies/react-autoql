35b7ec58e285279f536210f73a407cd9cf232864
Ben-CHATA <benwozak@gmail.com>
2022-03-08 09:37:16 -0700

    refactor: changed default prop

--- src/components/DataMessenger/DataMessenger.js
+++ src/components/DataMessenger/DataMessenger.js
@@ -118,7 +118,7 @@ export default class DataMessenger extends React.Component {
     maxMessages: undefined,
     introMessage: undefined,
     enableExploreQueriesTab: true,
-    enableNotificationsTab: true,
+    enableNotificationsTab: false,
     resizable: true,
     inputPlaceholder: undefined,
     queryQuickStartTopics: undefined,
