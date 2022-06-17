3a5c791faa7d1ebdd62944cb35e8ced2a186baec
Nikki Moore <nmoore@chata.ai>
2022-06-17 10:07:55 -0600

    do not show pointer cursor if drilldowns not supported

--- src/components/ChataTable/ChataTable.js
+++ src/components/ChataTable/ChataTable.js
@@ -39,6 +39,7 @@ export default class ChataTable extends React.Component {
       dataLoadError: (error) => {
         console.error(error)
       },
+      selectableCheck: () => false,
       layout: 'fitDataFill',
       textSize: '9px',
       clipboard: true,
