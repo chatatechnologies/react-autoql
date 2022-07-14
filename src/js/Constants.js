10d0733b54a89b2f664657015609ed67e7aa05ec
Nikki Moore <nmoore@chata.ai>
2022-07-14 16:12:18 -0600

    fix: date pivots omitting first row of data

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -22,20 +22,20 @@ export const CHART_TYPES = [
   'stacked_line',
 ]
 
-export const MONTH_NAMES = {
-  1: dayjs('2020-1-1').format('MMMM'),
-  2: dayjs('2020-2-2').format('MMMM'),
-  3: dayjs('2020-3-3').format('MMMM'),
-  4: dayjs('2020-4-4').format('MMMM'),
-  5: dayjs('2020-5-5').format('MMMM'),
-  6: dayjs('2020-6-6').format('MMMM'),
-  7: dayjs('2020-7-7').format('MMMM'),
-  8: dayjs('2020-8-8').format('MMMM'),
-  9: dayjs('2020-9-9').format('MMMM'),
-  10: dayjs('2020-10-10').format('MMMM'),
-  11: dayjs('2020-11-11').format('MMMM'),
-  12: dayjs('2020-12-12').format('MMMM'),
-}
+export const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
 
 export const WEEKDAY_NAMES = [
   'Sunday',
