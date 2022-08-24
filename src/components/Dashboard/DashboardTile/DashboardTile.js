c11796569581592c70d834f8439fd14d799fc0b9
Nikki Moore <nmoore@chata.ai>
2022-08-24 15:43:22 -0600

    test: add test for refreshLayout function

--- src/components/Dashboard/DashboardTile/DashboardTile.js
+++ src/components/Dashboard/DashboardTile/DashboardTile.js
@@ -44,7 +44,7 @@ import './DashboardTile.scss'
 
 let autoCompleteArray = []
 
-class DashboardTile extends React.Component {
+export class DashboardTile extends React.Component {
   constructor(props) {
     super(props)
 
@@ -80,9 +80,9 @@ class DashboardTile extends React.Component {
     dataFormatting: dataFormattingType,
     themeConfig: themeConfigType,
 
-    isEditing: PropTypes.bool.isRequired,
     tile: PropTypes.shape({}).isRequired,
-    deleteTile: PropTypes.func.isRequired,
+    isEditing: PropTypes.bool,
+    deleteTile: PropTypes.func,
     dataPageSize: PropTypes.number,
     queryResponse: PropTypes.shape({}),
     notExecutedText: PropTypes.string,
@@ -92,7 +92,7 @@ class DashboardTile extends React.Component {
     onCSVDownloadStart: PropTypes.func,
     onCSVDownloadProgress: PropTypes.func,
     onCSVDownloadFinish: PropTypes.func,
-    enableAjaxTableData: PropTypes.bool.isRequired,
+    enableAjaxTableData: PropTypes.bool,
   }
 
   static defaultProps = {
@@ -104,12 +104,15 @@ class DashboardTile extends React.Component {
 
     query: '',
     title: '',
+    isEditing: false,
     displayType: 'table',
     dataPageSize: undefined,
     queryValidationSelections: undefined,
     selectedSuggestion: undefined,
     notExecutedText: 'Hit "Execute" to run this dashboard',
     autoChartAggregations: true,
+    enableAjaxTableData: false,
+    deleteTile: () => {},
     onErrorCallback: () => {},
     onSuccessCallback: () => {},
     onCSVDownloadStart: () => {},
@@ -1220,8 +1223,8 @@ class DashboardTile extends React.Component {
           this.props.secondQueryResponse || this.props.queryResponse,
         tableConfigs: this.props.tile.secondDataConfig,
         onTableConfigChange: this.onSecondDataConfigChange,
-        queryValidationSelections: this.props.tile
-          .secondqueryValidationSelections,
+        queryValidationSelections:
+          this.props.tile.secondqueryValidationSelections,
         onSuggestionClick: this.onSecondSuggestionClick,
         selectedSuggestion: _get(this.props.tile, 'secondSelectedSuggestion'),
         reportProblemCallback: this.secondReportProblemCallback,
@@ -1310,6 +1313,7 @@ class DashboardTile extends React.Component {
           className={this.props.className}
           style={{ ...this.props.style }}
           data-grid={this.props.tile}
+          data-test="react-autoql-dashboard-tile"
           {...propsToPassToDragHandle}
         >
           {this.props.children}
@@ -1339,7 +1343,6 @@ export default React.forwardRef(({ style, className, key, ...props }, ref) => (
       {...props}
       ref={props.tileRef}
       className={`${props.innerDivClass} ${props.isEditing ? 'editing' : ''}`}
-      data-test="react-autoql-dashboard-tile"
     />
   </div>
 ))
