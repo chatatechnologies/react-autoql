f5eb89ad320dfc41dc62731f5dd2d55a19df627c
Nikki Moore <nmoore@chata.ai>
2022-10-20 15:49:37 -0600

    fix: remove subject filters for "change in"

--- src/components/DataExplorer/DataExplorerInput.js
+++ src/components/DataExplorer/DataExplorerInput.js
@@ -65,15 +65,10 @@ export default class DataExplorerInput extends React.Component {
 
   isAggSeed(subject) {
     return (
-      subject.display_name.toLowerCase().startsWith('monthly change in') ||
-      subject.display_name.toLowerCase().startsWith('yearly change in') ||
-      subject.display_name.toLowerCase().startsWith('weekly change in') ||
-      subject.display_name.toLowerCase().startsWith('daily change in') ||
       subject.display_name.toLowerCase().endsWith('by day') ||
       subject.display_name.toLowerCase().endsWith('by week') ||
       subject.display_name.toLowerCase().endsWith('by month') ||
-      subject.display_name.toLowerCase().endsWith('by year') ||
-      subject.display_name.toLowerCase().includes('change in')
+      subject.display_name.toLowerCase().endsWith('by year')
     )
   }
   fetchAllSubjects = () => {
