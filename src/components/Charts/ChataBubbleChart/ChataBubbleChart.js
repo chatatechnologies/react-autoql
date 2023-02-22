cb622df62fe692fc45f0f71f4c8ab9ff12879b14
Nikki Moore <nmoore@chata.ai>
2023-02-22 14:35:59 -0700

    fix: heatmap scale

--- src/components/Charts/ChataBubbleChart/ChataBubbleChart.js
+++ src/components/Charts/ChataBubbleChart/ChataBubbleChart.js
@@ -20,6 +20,7 @@ export default class ChataBubbleChart extends Component {
       columnIndex: props.stringColumnIndex,
       axis: 'x',
       innerPadding: 0.01,
+      outerPadding: 0.1,
     })
 
     this.yScale = getBandScale({
@@ -27,6 +28,7 @@ export default class ChataBubbleChart extends Component {
       domain: props.legendLabels.map((d) => d.label),
       axis: 'y',
       innerPadding: 0.01,
+      outerPadding: 0.1,
     })
   }
 
