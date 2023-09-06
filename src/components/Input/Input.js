1aca18fb801e7f7f8ec49a8674d74da6c248cc13
Nikki Moore <nmoore@chata.ai>
2023-09-06 10:32:47 -0600

    fix autocomplete prop

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -131,7 +131,7 @@ export default class Input extends React.Component {
             ) : (
               <div className='react-autoql-input-and-icon'>
                 <input
-                  autoComplete={false}
+                  autoComplete='off'
                   {...nativeProps}
                   ref={(r) => (this.inputRef = r)}
                   className={`react-autoql-input
