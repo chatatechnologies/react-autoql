b6be51d0c79f05345af60654cac860504f1a15c8
Alex Zhou <azhou@chata.ai>
2023-10-02 10:06:14 -0600

    remove queryText default props as it's not being used

--- src/components/Notifications/DataAlertSettings/AppearanceSection.js
+++ src/components/Notifications/DataAlertSettings/AppearanceSection.js
@@ -24,7 +24,6 @@ export default class AppearanceSection extends React.Component {
     titleInput: '',
     messageInput: '',
     showConditionStatement: true,
-    queryText: '',
     onTitleInputChange: () => {},
     onMessageInputChange: () => {},
   }
