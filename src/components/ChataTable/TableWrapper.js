ea575fc952691fa2cf8ac00414425ef6ea0b3dc0
Nikki Moore <nmoore@chata.ai>
2024-01-17 14:55:31 -0700

    _cloneDeep not defined

--- src/components/ChataTable/TableWrapper.js
+++ src/components/ChataTable/TableWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 as uuid } from 'uuid'
+import _cloneDeep from 'lodash.clonedeep'
 import { TabulatorFull as Tabulator } from 'tabulator-tables' //import Tabulator library
 
 // use Theme(s)
