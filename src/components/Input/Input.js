d451e20ce423e4867553ee01a7455b12e1cc9b22
eleon-chata-ai <eleon@chata.ai>
2025-02-06 12:10:52 -0700

    ANT-3444 - Replace is not a function for number columns, also fixed date column click

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -37,7 +37,7 @@ export default class Input extends React.Component {
     showArrow: PropTypes.bool,
     showSpinWheel: PropTypes.bool,
     disabled: PropTypes.bool,
-    errormessage: PropTypes.string
+    errormessage: PropTypes.string,
   }
 
   static defaultProps = {
@@ -292,9 +292,11 @@ export default class Input extends React.Component {
                 {icon && (
                   <Icon className={`react-autoql-input-icon ${this.state.focused ? ' focus' : ''}`} type={icon} />
                 )}
-                {this.props.errormessage &&
-                  <span id="input-error" className="error-message">{this.props.errormessage}</span>
-                }
+                {this.props.errormessage && (
+                  <span id='input-error' className='error-message'>
+                    {this.props.errormessage}
+                  </span>
+                )}
                 {this.props.datePicker ? this.renderDateRangePickerPopover() : null}
               </div>
             )}
