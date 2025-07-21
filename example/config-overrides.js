adc7fd230f7464aa7d9bac6011131c3fcfd29fdf
Alex Zhou <azhou@chata.ai>
2025-07-21 15:45:04 -0600

    try to fix

--- example/config-overrides.js
+++ example/config-overrides.js
@@ -8,29 +8,28 @@ module.exports = function override(config, env) {
       type: 'javascript/auto',
     })
 
-    // Find the babel-loader rule and update it to handle modern syntax
-    const babelRule = config.module.rules.find(rule => 
-      rule.test && rule.test.toString().includes('js') && 
-      rule.use && Array.isArray(rule.use) &&
-      rule.use.some(loader => loader.loader && loader.loader.includes('babel-loader'))
-    )
-
-    if (babelRule) {
-      const babelLoader = babelRule.use.find(loader => 
-        loader.loader && loader.loader.includes('babel-loader')
-      )
-      
-      if (babelLoader && babelLoader.options) {
-        // Add plugins for optional chaining and nullish coalescing
-        if (!babelLoader.options.plugins) {
-          babelLoader.options.plugins = []
-        }
-        babelLoader.options.plugins.push(
-          '@babel/plugin-transform-optional-chaining',
-          '@babel/plugin-transform-nullish-coalescing-operator'
-        )
-      }
-    }
+    // Add babel-loader rule for node_modules to handle modern syntax
+    config.module.rules.push({
+      test: /\.(js|mjs|jsx)$/,
+      include: /node_modules\/react-draggable/,
+      use: {
+        loader: require.resolve('babel-loader'),
+        options: {
+          babelrc: false,
+          configFile: false,
+          compact: false,
+          presets: [
+            ['@babel/preset-env', { modules: false }],
+          ],
+          plugins: [
+            '@babel/plugin-transform-optional-chaining',
+            '@babel/plugin-transform-nullish-coalescing-operator',
+          ],
+          cacheDirectory: true,
+          cacheCompression: false,
+        },
+      },
+    })
   }
 
   return config
