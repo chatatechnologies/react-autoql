7a969618463beee06bfb1af6018a7cef63b03111
Alex Zhou <azhou@chata.ai>
2025-09-16 10:30:13 -0600

    add back refresh interval button

--- src/components/DashboardToolbar/DashboardToolbar.js
+++ src/components/DashboardToolbar/DashboardToolbar.js
@@ -246,10 +246,9 @@ export class DashboardToolbarWithoutRef extends React.Component {
               {this.dashboardSlicingFeatureToggle && !this.props.isEditing && this.renderFilterInput()}
               {!this.props.isEditing ? (
                 <>
-                  {/* 
-				  This is the refresh interval button that is currently disabled
-				  TODO: Re-enable this button once the refresh interval is implemented
-				   <div
+                  This is the refresh interval button that is currently disabled TODO: Re-enable this button once the
+                  refresh interval is implemented
+                  <div
                     style={{ display: 'flex', alignItems: 'center', cursor: 'pointer', padding: '8px', opacity: 0.7 }}
                   >
                     <Icon
@@ -261,7 +260,7 @@ export class DashboardToolbarWithoutRef extends React.Component {
                     <span data-tooltip-content={this.getRefreshIntervalText()} data-tooltip-id={this.props.tooltipID}>
                       {this.getRefreshIntervalDisplay()}
                     </span>
-                  </div> */}
+                  </div>
                   <Button
                     iconOnly
                     icon='refresh'
