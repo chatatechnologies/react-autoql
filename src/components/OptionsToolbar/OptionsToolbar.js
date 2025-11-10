8df14ed154726fe320c5e5c6140e2f7350955b8c
eleon-chata-ai <eleon@chata.ai>
2025-11-10 15:55:20 -0700

    AQLP-352 - update code for more stable filter badge and add tests

--- src/components/OptionsToolbar/OptionsToolbar.js
+++ src/components/OptionsToolbar/OptionsToolbar.js
@@ -122,6 +122,14 @@ export class OptionsToolbar extends React.Component {
     clearTimeout(this.temporaryStateTimeout)
     clearTimeout(this.pivotTableCSVDownloadTimeout)
   }
+
+  countTabulatorHeaderFilters = (tabulatorHeaderFilters) => {
+    if (!tabulatorHeaderFilters) return 0
+    return Array.isArray(tabulatorHeaderFilters)
+      ? tabulatorHeaderFilters.length
+      : Object.keys(tabulatorHeaderFilters || {}).length
+  }
+
   renderOpenInNewBtn = () => {
     return (
       <Button
@@ -567,8 +575,10 @@ export class OptionsToolbar extends React.Component {
 
   renderFilterBtn = () => {
     const tabulatorHeaderFilters = this.props.responseRef?.getTabulatorHeaderFilters()
+    const tabulatorHeaderFiltersCount = this.countTabulatorHeaderFilters(tabulatorHeaderFilters)
+
     const isFiltered =
-      !!this.props.responseRef?.formattedTableParams?.filters?.length && !!tabulatorHeaderFilters?.length
+      !!this.props.responseRef?.formattedTableParams?.filters?.length && tabulatorHeaderFiltersCount > 0
     const displayType = this.props.responseRef?.state?.displayType
     const isTable = displayType === 'table'
 
