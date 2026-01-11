31d29c5c9f33f26a5de2dfca74139216fd1bc844
eleon-chata-ai <eleon@chata.ai>
2026-01-10 17:50:26 -0700

    AQLP-476 - AX_PIVOT_TABLE_CELLS is set too high

--- src/components/QueryOutput/QueryOutput.js
+++ src/components/QueryOutput/QueryOutput.js
@@ -100,7 +100,7 @@ export class QueryOutput extends React.Component {
     this.TOOLTIP_ID = `react-autoql-query-output-tooltip-${this.COMPONENT_KEY}`
     this.CHART_TOOLTIP_ID = `react-autoql-query-output-chart-tooltip-${this.COMPONENT_KEY}`
     this.ALLOW_NUMERIC_STRING_COLUMNS = true
-    this.MAX_PIVOT_TABLE_CELLS = 500000
+    this.MAX_PIVOT_TABLE_CELLS = 5000
 
     this.originalLegendState = {
       hiddenLegendLabels: [],
