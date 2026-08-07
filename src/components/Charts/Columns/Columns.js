325ad297a1a5e4c6b4e568e16f45060fe24ccaff
Nikki Moore <nmoore@chata.ai>
2026-08-07 11:17:18 -0600

    add minimum bar/column size for non-zero values

--- src/components/Charts/Columns/Columns.js
+++ src/components/Charts/Columns/Columns.js
@@ -10,6 +10,7 @@ import {
 } from '../chartPropHelpers'
 
 const [OPACITY_1, OPACITY_2, OPACITY_3] = getGradientOpacityStops('vertical')
+const MIN_BAR_SIZE = 1
 
 export default class Columns extends PureComponent {
   static propTypes = chartElementPropTypes
@@ -81,17 +82,20 @@ export default class Columns extends PureComponent {
               return null
             }
 
-            let y = value < 0 ? scaleZero(yScale) : yScale.getValue(value)
             let height = Math.abs(yScale.getValue(value) - scaleZero(yScale))
-
-            if (isNaN(height) || isNaN(value)) {
-              y = scaleZero(yScale)
+            if (isNaN(height)) {
               height = 0
+            }
+
+            if (value !== 0 && height > 0 && height < MIN_BAR_SIZE) {
+              height = MIN_BAR_SIZE
             }
 
             if (height < 0.05) {
               return null
             }
+
+            const y = value < 0 ? scaleZero(yScale) : scaleZero(yScale) - height
 
             const x0 = xScale.getValue(d[stringColumnIndex])
             const dX = visibleIndex * (this.barWidth + seriesGap)
